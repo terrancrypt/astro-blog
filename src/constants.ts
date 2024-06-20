a2c069e7ee0f376e9372d180b61d020975da91f4
terrancrypt <thaiphamngoctuong@gmail.com>
2024-06-20 18:00:15 +0700

    update pagination function

--- src/constants.ts
+++ src/constants.ts
@@ -2,4 +2,4 @@ export const SITE_TITLE = "TechPeople Blog";
 export const SITE_DESCRIPTION =
   "Articles about software development, programming, and technology.";
 export const HOMEPAGE_ARTICLE_LIMIT = 6;
-export const ARTICLES_PER_PAGE = 3;
+export const ARTICLES_PER_PAGE = 6;
