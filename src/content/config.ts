72d3c4165c2c484699abd172ca469e71b2e990cd
terrancrypt <thaiphamngoctuong@gmail.com>
2024-06-20 14:05:26 +0700

    define collection config

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,17 @@
-import { defineCollection } from "astro:content";
+import { z, defineCollection } from "astro:content";
 
-const blogCollection = defineCollection({});
+const blogCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    schema: z.object({
+      title: z.string(),
+      pubDate: z.date(),
+      author: z.string(),
+      image: z.string(),
+      tags: z.array(z.string()),
+    }),
+  }),
+});
 
 export const conllections = {
   blog: blogCollection,
